6a20a42dd04c32a630a28420e777bfe0870ced16
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-07-01 18:20:21 +0530

    new file added

---
+++ JavaStrings/StringContains.java
@@ -0,0 +1,15 @@
+package JavaStrings;
+
+public class StringContains {
+    public static void main(String[] args) {
+        String str = "GeeksforGeeks";
+        String str2 = "Geeks";
+
+        if(str.contains(str2)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
