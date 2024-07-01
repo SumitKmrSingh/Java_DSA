eb16df136125156a6b6c263b44ba07e01f68c656
SumitKmrSingh <sumitkumar203411@gmail.com>
2024-07-01 18:25:49 +0530

    new file added

---
+++ JavaStrings/IndexOf.java
@@ -0,0 +1,13 @@
+package JavaStrings;
+
+public class IndexOf {
+    public static void main(String[] args) {
+        String str1 = "GeeksforGeeks";
+        String str2 = "Geeks";
+
+        System.out.println(str1.indexOf(str2));
+
+        System.out.println(str1.indexOf(str2, 1));
+
+    }
+}
